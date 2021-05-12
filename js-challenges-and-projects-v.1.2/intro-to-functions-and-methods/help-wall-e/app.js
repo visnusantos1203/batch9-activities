127bc3f8ac6fee0b88b0777c3e76ae052bedb6e0
Visnu Santosgit config --global user.email visnu.santos1203@gmail,com <visnu.santos1203@gmail.com>
2021-05-12 21:21:12 +0800

    Finished today's activities

--- js-challenges-and-projects-v.1.2/intro-to-functions-and-methods/help-wall-e/app.js
+++ js-challenges-and-projects-v.1.2/intro-to-functions-and-methods/help-wall-e/app.js
@@ -4,15 +4,14 @@ const robot = document.querySelector('.robot')
 
 function moveRobot() {
     //add code here
-    robot.style.marginLeft = "45vw";
-}
+    var currentLoc = window.getComputedStyle(robot).marginLeft;
+    var currentLoc2 = parseInt(currentLoc);
+    const additionalSpace = 50;
+    var newLoc = currentLoc2 + additionalSpace;
+
+    robot.style.marginLeft= `${newLoc}px`;
+    console.log(currentLoc2);}
+    
 robot.addEventListener('click', moveRobot)
 
 //awit
-
-
-function moveRobot() {
-    //add code here
-    robot.style.marginLeft = "45vw";
-}
-robot.addEventListener('click', moveRobot)
--- js-challenges-and-projects-v.1.2/intro-to-functions-and-methods/schools-out/app.js
+++ js-challenges-and-projects-v.1.2/intro-to-functions-and-methods/schools-out/app.js
@@ -1,9 +1,7 @@
-function sendText( ) {
+function sendText(name, lastName ) {
     //Write your code here.
+    console.log(`Good day ${name} ${lastName}! I would like to inform you that our classes are suspended today. Keep safe and see you tomorrow!`);
 }
-sendText()
-
-
 sendText('Jose', 'Rizal')
 sendText('Manny', 'Pacquiao')
 sendText('Robert', 'Rodriguez')
