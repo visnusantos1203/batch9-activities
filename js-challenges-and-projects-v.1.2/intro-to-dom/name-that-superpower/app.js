2b237375fdbb4e2b02bbe576ad4b90c8f5e14821
Visnu Santosgit config --global user.email visnu.santos1203@gmail,com <visnu.santos1203@gmail.com>
2021-05-14 23:17:23 +0800

    Done with today's activities

--- js-challenges-and-projects-v.1.2/intro-to-dom/name-that-superpower/app.js
+++ js-challenges-and-projects-v.1.2/intro-to-dom/name-that-superpower/app.js
@@ -5,3 +5,18 @@ var justiceLeague = [
     {name: "The Flash", superpower: "Super speed"}, 
     {name: "Green Lantern", superpower: "Super ring"}
 ]
+
+
+for(let i = 0; i < justiceLeague.length; i++){
+    var league = document.getElementById("league");
+    var superHero = document.createElement("li");
+    superHero.innerText = `Name: ${justiceLeague[i].name} Superpower: ${justiceLeague[i].superpower}`
+    league.append(superHero);
+    console.log(superHero);
+}
+
+document.getElementById("h1").addEventListener("keypress", onKeyPress);
+
+function onKeyPress(e){
+    console.log(`You pressed ${e.key}`)
+}
