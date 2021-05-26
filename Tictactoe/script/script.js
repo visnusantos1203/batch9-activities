a4c56106186361564ad8fd04d97dfecf64092d6f
Visnu Santosgit config --global user.email visnu.santos1203@gmail,com <visnu.santos1203@gmail.com>
2021-05-27 00:43:21 +0800

    added modal in html, Tried to code game history

--- Tictactoe/script/script.js
+++ Tictactoe/script/script.js
@@ -1,11 +1,11 @@
 const statusDisplay = document.querySelector(".game-status")
+const winner = document.getElementById("winner");
 let gameActive = true;
 let currentPlayer = "X";
 let gameState = ["", "", "", "", "", "", "", "", ""];
-const winningMessage = () => `Player ${currentPlayer} has won!`;
+const winningMessage = () => `${currentPlayer} has won!`;
 const drawMessage = () => `Game ended in a draw!`;
 const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
-
 statusDisplay.innerHTML = currentPlayerTurn();
 
 function handleCellClick(clickedCellEvent) {
@@ -13,12 +13,23 @@ function handleCellClick(clickedCellEvent) {
     const clickedCellIndex = parseInt(
         clickedCell.getAttribute('data-cell-index')
     );
+    //ipupush sa index ng gameHistory gamit yung clickedCellIndex yung current player
+
+    for(let i = 0; i < gameHistory.length; i++){
+        if(i === clickedCellIndex){
+        gameHistory[i] = currentPlayer;
+        //console.log(clickedCellIndex);
+        //console.log(gameHistory);
+        }
+    };
+
     if (gameState[clickedCellIndex] !== "" || !gameActive) {
          return;
     }
     handleCellPlayed(clickedCell, clickedCellIndex);
     handleResultValidation();
 }
+
 
 function handleCellPlayed(clickedCell, clickedCellIndex) {
     gameState[clickedCellIndex] = currentPlayer;
@@ -43,7 +54,7 @@ function handleResultValidation() {
         let a = gameState[winCondition[0]];
         let b = gameState[winCondition[1]];
         let c = gameState[winCondition[2]];
-        console.log(winCondition);
+        //console.log(winCondition);
 
         if (a === '' || b === '' || c === '') {
             continue;
@@ -54,7 +65,8 @@ function handleResultValidation() {
         }
     }
     if (roundWon) {
-        statusDisplay.innerHTML = winningMessage();
+        statusDisplay.innerHTML = "Game Over!";
+        winner.innerHTML = winningMessage();
         gameActive = false;
 
         return;
@@ -89,9 +101,32 @@ function onHover(e){
         e.target.textContent = "O";
     }
 }
+// try lang to
+let gameHistory = ["", "", "", "", "", "", "", "", ""];
+
+function gameHis(clickedCellEvent) {
+    gameHistory.forEach(myFUnction);
+
+    const clickedCelll = clickedCellEvent.target;
+    const clickedCelllIndex = parseInt(
+        clickedCelll.getAttribute('data-cell-index')
+    );
+    //ipupush sa index ng gameHistory gamit yung clickedCellIndex yung current player
+
+    for(let i = 0; i < gameHistory.length; i++){
+        if(i === clickedCelllIndex){
+        gameHistory[i] = currentPlayer;
+        console.log(clickedCelllIndex);
+        console.log(gameHistory);
+        }
+    };
+    function myFUnction(item, index){
+        if(clickedCelllIndex === index){
+            clickedCellIndex.innerHTML = item;
+        }
+    };
+};  gameHis();  
+//try lang to
 
 document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
 document.querySelector('#restartBtn').addEventListener('click', handleRestartGame);
-
-
-
